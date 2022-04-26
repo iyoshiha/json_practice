ab8838336def854523a61fc3a9a03ee8ad16afe3
iyoshiha <yoshi3co@gmail.com>
2022-04-26 20:44:19 +0900

    document return value

---
+++ myscript.js
@@ -0,0 +1,13 @@
+const todos = [];
+function showTodoList() {
+    const list = document.getElementById('todoList');
+    console.log(list);
+    var cody = {
+        living: true,
+        age: 33,
+    }
+    console.log(cody);
+    console.log(document);
+
+}
+
