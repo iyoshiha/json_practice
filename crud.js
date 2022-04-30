b47fa6799de0491b782ee0f8c8dbbe52197fe33c
iyoshiha <yoshi3co@gmail.com>
2022-04-30 16:36:47 +0900

    j

--- crud.js
+++ crud.js
@@ -1,4 +1,5 @@
 const sites=[];
+const url = 'http://localhost:3000/sites';
 
 function createData() {
     const newSiteName = document.getElementById("siteName").value;
@@ -12,20 +13,49 @@ function createData() {
         url : newSiteUrl
     }
 
-    sites.push(siteInfo);
-
+    // sites.push(siteInfo);
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type':'application/json'
+        },
+        body: JSON.stringify(siteInfo)
+    })
+    .then(()=> {
     document.getElementById('siteName').value = '';
     document.getElementById("siteUrl").value = '';
+    })
+    .catch((err) => console.log(err))
 }
 
+
+var cody = {
+    age:10,
+    living:true,
+    gender:'male'
+}
+var elsa = {
+    age:20,
+    gender:'female'
+}
+var aryObj = [cody, elsa];
+        console.log(JSON.stringify(aryObj));
 
 function readData() {
     const list = document.getElementById('dataList');
     list.innerHTML = '';
 
-    for (let i = 0; i < sites.length; i++) {
-        list.innerHTML += `<li>${i} ${sites[i].name} ${sites[i].url}</li>`
-    }
+    // for (let i = 0; i < sites.length; i++) {
+    //     list.innerHTML += `<li>${i} ${sites[i].name} ${sites[i].url}</li>`
+    // }
+    fetch(url)
+    .then((res)=>res.json())
+    .then((sites)=>{
+        for (let i = 0; i < sites.length; i++) {
+            console.log(`${sites[i].name}`);
+            list.innerHTML += `<li>${sites[i].id} ${sites[i].name} ${sites[i].url}</li>`
+        }
+    })
 }
 
 function updateData() {
@@ -35,10 +65,17 @@ function updateData() {
 
     if (updateId === '' || updateName === '' || updateUrl === '') return; 
 
-    sites[Number(updateId)] = {
-        name: updateName,
-        url: updateUrl
-    };
+    // sites[Number(updateId)] = {
+    //     name: updateName,
+    //     url: updateUrl
+    // };
+    fetch(url, {
+        method: 'DELETE',
+        headers: {
+            'Content-type':'application/json'
+        },
+        body: JSON.stringify(siteInfo)
+    })
 
     document.getElementById('updateId').value = '';
     document.getElementById('updateName').value = '';
@@ -49,6 +86,11 @@ function deleteData() {
     const deleteId = document.getElementById('deleteId').value;
     if (deleteId === '') return;
 
-    sites.splice(Number(deleteId), 1);
+    fetch(url + '/' +deleteId, {
+        method: 'DELETE'
+    })
+    .then(() => {
     document.getElementById("deleteId").value = '';
+    })
+    .catch((err) => console.log(err))
 }
